d325111ac18f38b7a54ed857e3d8d56d50c2749e
Marsyanda <marsyandafirlyandita@gmail.com>
2025-03-01 11:43:33 +0700

    first commit

---
+++ script/main.js
@@ -0,0 +1,21 @@
+// toggle & responsive navigation
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+// clear from before undo
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+};
+
+
